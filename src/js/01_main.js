8b98210bf4970ef9aa3f62ee4194b19adb9ecc4b
Максим Циркулев <de_animator@mail.ru>
2024-11-13 12:38:40 +0300

    upd md project

---
+++ src/js/01_main.js
@@ -0,0 +1,76 @@
+const body = document.body;
+// Получаем элемент навигации
+const nav = document.querySelector('[data-element="nav"]');
+
+// Определяем начальную позицию навигации
+const initialNavPosition = nav.offsetTop;
+
+// прокрутка статьи
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function(e) {
+		e.preventDefault();
+		let destination = document.querySelector(this.getAttribute('href'));
+		const header = document.querySelector('[data-element="header"]');
+		if (destination) {
+			let headerHeight = (header.offsetHeight + 30);
+			let topOfDestination = destination.getBoundingClientRect().top + window.scrollY - headerHeight;
+			window.scrollTo({ top: topOfDestination, behavior: "smooth" });
+		}
+	});
+});
+
+[].forEach.call( document.querySelectorAll('.form__input--tel'), function(input) {
+	var keyCode;
+	function mask(event) {
+		event.keyCode && (keyCode = event.keyCode);
+		var pos = this.selectionStart;
+		if (pos < 3) event.preventDefault();
+		var matrix = "+7 (___) ___ __ __",
+			i = 0,
+			def = matrix.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, ""),
+			new_value = matrix.replace(/[_\d]/g, function(a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+			});
+		i = new_value.indexOf("_");
+		if (i != -1) {
+			i < 5 && (i = 3);
+			new_value = new_value.slice(0, i)
+		}
+		var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+			function(a) {
+				return "\\d{1," + a.length + "}"
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+		if (event.type == "blur" && this.value.length < 5) this.value = ""
+	}
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
+});
+
+$('.slider').slick({
+	dots: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 2,
+	adaptiveHeight: true
+});
+
+// Функция для изменения классов
+const updateNavClasses = () => {
+	const scrollPosition = window.scrollY;
+
+	// Если навигация скрылась из вида
+	if (scrollPosition > initialNavPosition) {
+		body.classList.add('is-scroll');
+	} else {
+		body.classList.remove('is-scroll');
+	}
+}
+
+updateNavClasses();
+// Навешиваем обработчик события прокрутки
+window.addEventListener('scroll', updateNavClasses);
